906e85438b4dca8ab6f83fd3441f58e35c39ab49
eliasaronson <elias.aronson@gmail.com>
2020-05-14 11:54:09 +0200

    Seems to be working now. Plot option 5 is not complete.

--- SIRS_test_Moore.py
+++ SIRS_test_Moore.py
@@ -19,6 +19,8 @@ import copy
 
 periodic = False
 
+global verbal
+verbal = 4
 
 if periodic:
     bType = cv2.BORDER_WRAP
@@ -26,40 +28,45 @@ else:
     bType = cv2.BORDER_REFLECT
 
 
-def Diffuse(inf, sus,D,h=0.01):
+def Diffuse(inf, h=0.01):
     tempInf = np.zeros(((xSize + 2), (ySize + 2)))
     tempInf[1:(xSize+1), 1:(ySize+1)] = np.copy(inf)
 
-    tempSus = np.zeros(((xSize + 2), (ySize + 2)))
-    tempSus[1:(xSize+1), 1:(ySize+1)] = np.copy(sus)
+    retInf = np.zeros((xSize, ySize))
+
+    if verbal == 1 or verbal == 1.1:
+        print("tempInf: \n", tempInf)
 
     for i, row in enumerate(inf):
         i += 1
+
         for j, col in enumerate(row):
             j += 1
-            neighb = tempInf[(i-1):(i+1), (j-1):(j+1)]
-            diff = (np.sum(neighb*D) - tempInf[i, j]*D)
-            tempInf[i, j] += diff 
-            tempSus[i, j] += -diff
 
-    retInf = np.copy(tempInf[1:(xSize+1), 1:(ySize+1)])
-    retSus = np.copy(tempSus[1:(xSize+1), 1:(ySize+1)])
+            neighb = np.copy(tempInf[(i-1):(i+2), (j-1):(j+2)])
+            neighb[1, 1] = 0
+            diff = (np.sum(neighb))
+            if verbal == 1:
+                print("diff: \n", diff)
+            #if(tempInf[i, j] + diff) < 0:
+            #    diff = 0
 
-    print()
-    print(retInf)
-    print(tempInf)
-    print(retSus)
-    print(tempInf)
+            retInf[(i-1), (j-1)] = diff
 
-    return retInf, retSus
+    if verbal == 1 or verbal == 1.1:
+        print()
+        print("retInf: \n", retInf)
+        print("tempInf: \n", tempInf)
 
-def susStep(sus, inf, rem, alpha, gamma, B, d, my, h=0.01):
-    return sus + (B + gamma*rem - d*sus -alpha*inf*(1-my*inf)*sus)*h
+    return retInf
 
-def infStep(sus, inf, alpha, beta, d, my, h=0.01):
-    return inf + (alpha*inf*(1-my*inf)*sus - (beta + d)*inf)*h
+def susStep(sus, inf, rem, alpha, gamma, B, d, my, diff, h=0.01):
+    return sus + (B + gamma*rem - d*sus -alpha*inf*(1-my*inf)*sus - (diff * sus))*h
 
-def remStep(rem, inf, beta, gamma, d,h=0.01):
+def infStep(sus, inf, alpha, beta, d, my, diff, h=0.01):
+    return inf + (alpha*inf*(1-my*inf)*sus - (beta + d)*inf + (diff * sus))*h
+
+def remStep(rem, inf, beta, gamma, d, h=0.01):
     return rem + (beta*inf - (gamma + d)*rem)*h
 
 
@@ -71,31 +78,29 @@ def timeStep(susMat, infMat, remMat, alpha, beta, gamma, B, d, my, Ds, Di, Dr, h
     tempInf = np.copy(infMat)
     tempRem = np.copy(remMat)
 
-    tempSus = susStep(susMat, infMat, remMat, alpha, gamma, B, d, my, h)
-    tempInf = infStep(susMat, infMat, alpha, beta, d, my, h)
+    diff = Ds * Diffuse(tempInf)
+
+    tempSus = susStep(susMat, infMat, remMat, alpha, gamma, B, d, my, diff, h)
+    tempInf = infStep(susMat, infMat, alpha, beta, d, my, diff, h)
     tempRem = remStep(remMat, infMat, beta, gamma, d, h)
 
-            
-    if Ds+Di+Dr != 0:
-        tempInf, tempSus, = Diffuse(tempInf, tempSus, Ds, h)
-
-    return tempSus, tempInf, tempRem                        
+    return tempSus, tempInf, tempRem
 
 
-option = 3
+option = 1
 
 xSize = 3
 ySize = 3
-iters = 1000
+iters = 10000
 beta = 0.1           #1/21
 alpha = 0.2  #2.5 / beta
 gamma = 0.001
 B = 0.00
 d = 0.00
 my = 0.5
-Ds = 0.005
-Di = 0.005
-Dr = 0.005
+Ds = 0.05
+Di = 0.0
+Dr = 0.0
 h = 0.05
 
 susMat = np.ones((xSize, ySize, iters))
@@ -110,7 +115,7 @@ for i in range(iters-1):
     tempSus, tempInf, tempRem = timeStep(susMat[:,:,i], infMat[:,:,i],
             remMat[:,:,i], alpha, beta, gamma, B, d, my, Ds, Di, Dr, h)
 
-    susMat[:,:,(i+1)] = tempSus 
+    susMat[:,:,(i+1)] = tempSus
     infMat[:,:,(i+1)] = tempInf
     remMat[:,:,(i+1)] = tempRem
 
@@ -122,8 +127,21 @@ if option == 0:
 if option == 1:
     for n in range(xSize):
         for m in range (ySize):
-            plt.plot(susMat[n, m, :])
+            c1 = [((n + m)/(np.max(xSize) + np.max(ySize))), 0, 0]
+            c2 = [0, ((n + m)/(np.max(xSize) + np.max(ySize))), 0]
+            c3 = [0, 0, ((n + m)/(np.max(xSize) + np.max(ySize)))]
+
+            plt.plot(susMat[n, m, :], color = c1)
+            plt.plot(infMat[n, m, :], color = c2)
+            plt.plot(remMat[n, m, :], color = c3)
+
+    plt.legend(["Susceptible", "Infected", "Removed"])
     plt.show()
+    if verbal == 4:
+        print("Sus: \n", susMat[:, :, -1])
+        print("Inf: \n", infMat[:, :, -1])
+        print("Rem: \n", remMat[:, :, -1])
+
 if option == 2:
     plt.matshow(susMat[:, :, -1])
     plt.show()
@@ -131,25 +149,105 @@ if option == 2:
 if option == 3:
     fig = plt.figure()
 
-    pltMat = infMat
-    def f(time):
-        return pltMat[:, :, time]
+    def f1(time):
+        return susMat[:, :, time]
+
+    def f2(time):
+        return infMat[:, :, time]
 
     time = -1;
 
-    im = plt.imshow(f(time), animated=True, vmin=np.min(pltMat),
-            vmax=np.max(pltMat))
+    plt.subplot(1, 2, 1)
+    im1 = plt.imshow(f1(time), animated=True, vmin=np.min(susMat), vmax=np.max(susMat))
+    plt.title('Susceptibale')
+
+    plt.subplot(1, 2, 2)
+    im2 = plt.imshow(f2(time), animated=True, vmin=np.min(infMat), vmax=np.max(infMat))
+    plt.title('Infected')
 
     def updatefig(*args):
         global time
         time += 1
-        if time > iters:
+        if time >= iters:
             time = 0
         print(time)
-        v = f(time)
+        v = f1(time)
         print(v)
-        im.set_array(f(time))
-        return im,
+        im1.set_array(f1(time))
+        im2.set_array(f2(time))
+        return im1, im2,
 
     ani = animation.FuncAnimation(fig, updatefig, frames=10, interval=0.1, blit=True)
-    plt.show()       
+    plt.show()
+
+if option == 4:
+    fig = plt.figure()
+
+    def f1(time):
+        return susMat[:, :, time]
+
+    def f2(time):
+        return infMat[:, :, time]
+
+    def f3(time):
+        return remMat[:, :, time]
+
+    def iniPlot():
+        pS = np.empty((xSize, ySize), dtype=object)
+        pI = np.empty((xSize, ySize), dtype=object)
+        pR = np.empty((xSize, ySize), dtype=object)
+        for n in range(xSize):
+            for m in range (ySize):
+                c1 = [((n + m)/(np.max(xSize) + np.max(ySize))), 0, 0]
+                c2 = [0, ((n + m)/(np.max(xSize) + np.max(ySize))), 0]
+                c3 = [0, 0, ((n + m)/(np.max(xSize) + np.max(ySize)))]
+
+                pS[n, m] = plt.plot([np.nan] * (iters - 1), color = c1)
+                pI[n, m] = plt.plot([np.nan] * (iters - 1), color = c2)
+                pR[n, m] = plt.plot([np.nan] * (iters - 1), color = c3)
+
+        plt.legend(["Susceptible", "Infected", "Removed"])
+        return pS, pI, pR
+
+    def updPlot(time):
+        for n in range(xSize):
+            for m in range (ySize):
+                ydataS = susMat[n, m, 0:time]
+                pS[n, m][0].set_ydata()
+                pI[n, m][0].set_ydata(infMat[n, m, time])
+                pR[n, m][0].set_ydata(remMat[n, m, time])
+        return pS, pI, pR
+
+    time = -1;
+
+    plt.subplot(2, 2, 1)
+    im1 = plt.imshow(f1(time), animated=True, vmin=np.min(susMat), vmax=np.max(susMat))
+    plt.title('Susceptibale')
+
+    plt.subplot(2, 2, 2)
+    im2 = plt.imshow(f2(time), animated=True, vmin=np.min(infMat), vmax=np.max(infMat))
+    plt.title('Infected')
+
+    plt.subplot(2, 2, 3)
+    im3 = plt.imshow(f2(time), animated=True, vmin=np.min(remMat), vmax=np.max(remMat))
+    plt.title('Infected')
+
+    plt.subplot(2, 2, 4)
+    pS, pI, pR = iniPlot()
+
+    def updatefig(*args):
+        global time
+        time += 1
+        if time >= iters:
+            time = 0
+        print(time)
+        v = f1(time)
+        print(v)
+        im1.set_array(f1(time))
+        im2.set_array(f2(time))
+        im3.set_array(f3(time))
+        updPlot(time)
+        return im1, im2, im3, pS[0, 0][0],
+
+    ani = animation.FuncAnimation(fig, updatefig, frames=10, interval=0.1, blit=True)
+    plt.show()
